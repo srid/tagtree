ead2315a5faf28d7d59943bc78685b43c9169371
Sridhar Ratnakumar <srid@srid.ca>
2021-06-09 18:55:55 -0400

    Import from neuron

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# haskell-template
+# tagtree
 
-Haskell project template optimized for a fully reproducible and friendly development environment. Based on [Nix](https://notes.srid.ca/haskell-nix) + [Flakes](https://serokell.io/blog/practical-nix-flakes) + VSCode ([HLS](https://github.com/haskell/haskell-language-server)) + [ormolu](https://github.com/tweag/ormolu) autoformatting + [Relude](https://github.com/kowainik/relude#relude) as Prelude.
+Haskell library for representing hierarchical tags, such as `foo/bar/qux` and and searching them based on path patterns.
 
-## Getting Started
+## Developing
 
 - [Install Nix](https://nixos.org/download.html) & [enable Flakes](https://nixos.wiki/wiki/Flakes)
 - Run `nix-shell --run haskell-language-server` to sanity check your environment 
@@ -13,14 +13,3 @@ Haskell project template optimized for a fully reproducible and friendly develop
 
 All but the final step need to be done only once.
 
-Then, before using it for real,
-
-- Rename all occurrences of `haskell-template` to `myproject`, as well as rename the cabal file to `myproject.cabal`. 
-- Run `git add . && git commit -m rename` followed by `nix develop` (or `bin/run`) to verify that everything continues to work.
-
-## Other templates
-
-Some related templates include,
-
-- [Serokell's Flake template](https://github.com/serokell/templates/tree/master/haskell-cabal2nix)
-  - [Same, but using haskell.nix](https://github.com/serokell/templates/pull/2)
---
+++ src/Data/TagTree.hs
@@ -0,0 +1,171 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralisedNewtypeDeriving #-}
+
+module Data.TagTree
+  ( Tag (..),
+    TagPattern (unTagPattern),
+    TagNode (..),
+    Query (..),
+    mkDefaultTagQuery,
+    mkTagPattern,
+    mkTagPatternFromTag,
+    tagMatch,
+    matchTagQuery,
+    matchTagQueryMulti,
+    tagTree,
+    foldTagTree,
+    constructTag,
+  )
+where
+
+import Control.Monad.Combinators.NonEmpty (sepBy1)
+import Data.Aeson (FromJSON, ToJSON)
+import qualified Data.Map.Strict as Map
+import Data.TagTree.PathTree (annotatePathsWith, foldSingleParentsWith, mkTreeFromPaths)
+import qualified Data.Text as T
+import Data.Tree (Forest)
+import System.FilePattern (FilePattern, (?==))
+import qualified Text.Megaparsec as M
+import qualified Text.Megaparsec.Char as M
+import Text.Show (Show (show))
+
+-- | A hierarchical tag
+--
+-- Tag nodes are separated by @/@
+newtype Tag = Tag {unTag :: Text}
+  deriving (Eq, Ord, Show, Generic)
+  deriving newtype
+    ( ToJSON,
+      FromJSON
+    )
+
+--------------
+-- Tag Pattern
+---------------
+
+-- | A glob-based pattern to match hierarchical tags
+--
+-- For example, the pattern
+--
+-- > foo/**
+--
+-- matches both the following
+--
+-- > foo/bar/baz
+-- > foo/baz
+newtype TagPattern = TagPattern {unTagPattern :: FilePattern}
+  deriving
+    ( Eq,
+      Ord,
+      Show,
+      Generic
+    )
+  deriving newtype
+    ( ToJSON,
+      FromJSON
+    )
+
+mkTagPattern :: Text -> TagPattern
+mkTagPattern =
+  TagPattern . toString
+
+mkTagPatternFromTag :: Tag -> TagPattern
+mkTagPatternFromTag (Tag t) =
+  TagPattern $ toString t
+
+data Query
+  = Query_And (NonEmpty TagPattern)
+  | Query_Or (NonEmpty TagPattern)
+  | Query_All
+  deriving (Eq, Ord, Generic)
+  deriving anyclass
+    ( ToJSON,
+      FromJSON
+    )
+
+mkDefaultTagQuery :: [TagPattern] -> Query
+mkDefaultTagQuery = maybe Query_All Query_Or . nonEmpty
+
+instance Show Query where
+  show = \case
+    Query_And (toList -> pats) ->
+      toString $ T.intercalate ", and " (fmap (toText . unTagPattern) pats)
+    Query_Or (toList -> pats) ->
+      toString $ T.intercalate ", or " (fmap (toText . unTagPattern) pats)
+    Query_All ->
+      "No particular tag"
+
+tagMatch :: TagPattern -> Tag -> Bool
+tagMatch (TagPattern pat) (Tag tag) =
+  pat ?== toString tag
+
+-- TODO: Use step from https://hackage.haskell.org/package/filepattern-0.1.2/docs/System-FilePattern.html#v:step
+-- for efficient matching.
+matchTagQuery :: Tag -> Query -> Bool
+matchTagQuery t = \case
+  Query_And pats -> all (`tagMatch` t) pats
+  Query_Or pats -> any (`tagMatch` t) pats
+  Query_All -> True
+
+matchTagQueryMulti :: [Tag] -> Query -> Bool
+matchTagQueryMulti tags = \case
+  Query_And pats -> any (\t -> all (`tagMatch` t) pats) tags
+  Query_Or pats -> any (\t -> any (`tagMatch` t) pats) tags
+  Query_All -> True
+
+-----------
+-- Tag Tree
+-----------
+
+-- | An individual component of a hierarchical tag
+--
+-- The following hierarchical tag,
+--
+-- > foo/bar/baz
+--
+-- has three tag nodes: @foo@, @bar@ and @baz@
+newtype TagNode = TagNode {unTagNode :: Text}
+  deriving (Eq, Show, Ord, Generic)
+  deriving newtype (ToJSON)
+
+deconstructTag :: HasCallStack => Tag -> NonEmpty TagNode
+deconstructTag (Tag s) =
+  either error id $ parse tagParser (toString s) s
+  where
+    tagParser :: Parser (NonEmpty TagNode)
+    tagParser =
+      nodeParser `sepBy1` M.char '/'
+    nodeParser :: Parser TagNode
+    nodeParser =
+      TagNode . toText <$> M.some (M.anySingleBut '/')
+
+constructTag :: NonEmpty TagNode -> Tag
+constructTag (fmap unTagNode . toList -> nodes) =
+  Tag $ T.intercalate "/" nodes
+
+-- | Construct the tree from a list of hierarchical tags
+tagTree :: ann ~ Natural => Map Tag ann -> Forest (TagNode, ann)
+tagTree tags =
+  fmap (annotatePathsWith $ countFor tags) $
+    mkTreeFromPaths $
+      toList . deconstructTag
+        <$> Map.keys tags
+  where
+    countFor tags' path =
+      fromMaybe 0 $ Map.lookup (constructTag path) tags'
+
+foldTagTree :: ann ~ Natural => Forest (TagNode, ann) -> Forest (NonEmpty TagNode, ann)
+foldTagTree tree =
+  foldSingleParentsWith foldNodes <$> fmap (fmap (first (:| []))) tree
+  where
+    foldNodes (parent, 0) (child, count) = Just (parent <> child, count)
+    foldNodes _ _ = Nothing
+
+type Parser a = M.Parsec Void Text a
+
+parse :: Parser a -> String -> Text -> Either Text a
+parse p fn s =
+  first (toText . M.errorBundlePretty) $
+    M.parse (p <* M.eof) fn s
